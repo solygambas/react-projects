e71cef3e8642867b8511711d5f5c8e6fbdc8d174
solygambas <solygambas@gmail.com>
2021-02-10 14:25:11 +0100

    add framer motion setup

---
+++ src/components/Home.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+
+const Home = () => {
+  return (
+    <motion.div
+      className="home container"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ delay: 1.5, duration: 1.5 }}
+    >
+      <h2>Welcome to Pizza Joint</h2>
+      <Link to="/base">
+        <motion.button animate={{}}>Create Your Pizza</motion.button>
+      </Link>
+    </motion.div>
+  );
+};
+
+export default Home;
