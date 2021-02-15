45482977bebd8e1f550f59aa2239d764ed338e54
solygambas <solygambas@gmail.com>
2021-02-15 12:20:18 +0100

    add pagination and readme

--- README.md
+++ README.md
@@ -8,6 +8,10 @@ A web app about Star Wars to understand React Query basics.
 
 ## Features
 
--
+- setting up a React project with React Query 3.
+- using QueryClientProvider as a wrapper.
+- fetching data from Swapi with useQuery.
+- handling query variables and pagination with keepPreviousData.
+- debugging with React Query Devtools.
 
 Based on [React Query Tutorial](https://www.youtube.com/watch?v=x1rQ61otgtU&list=PL4cUxeGkcC9jpi7Ptjl5b50p9gLjOFani) by Shaun Pelling - The Net Ninja (2020).
--- src/components/Planets.js
+++ src/components/Planets.js
@@ -1,30 +1,62 @@
+import { useState } from "react";
 import { useQuery } from "react-query";
 import Planet from "./Planet";
 
-const fetchPlanets = async () => {
-  const res = await fetch("https://swapi.dev/api/planets/");
+// Reference: https://github.com/tannerlinsley/react-query/tree/master/examples/pagination
+
+const fetchPlanets = async (page = 1) => {
+  const res = await fetch(`http://swapi.dev/api/planets/?page=${page}`);
   return res.json();
 };
 
 const Planets = () => {
-  const { data, status } = useQuery("planets", fetchPlanets, {
-    staleTime: 5000,
-    // cacheTime: 10,
-    // onSuccess: () => console.log("data fetched with no problemo"),
-  });
-  console.log(data);
+  const [page, setPage] = useState(1);
+  const { status, data, error, isFetching, isPreviousData } = useQuery(
+    ["planets", page],
+    () => fetchPlanets(page),
+    {
+      keepPreviousData: true,
+      staleTime: 5000,
+      // cacheTime: 10,
+      // onSuccess: () => console.log("data fetched with no problemo"),
+    }
+  );
+
   return (
     <div>
       <h2>Planets</h2>
       {status === "loading" && <div>Loading...</div>}
-      {status === "error" && <div>Error fetching data</div>}
+      {status === "error" && <div>Error: {error.message}</div>}
       {status === "success" && (
-        <div>
-          {data.results.map((planet) => (
-            <Planet key={planet.name} planet={planet} />
-          ))}
-        </div>
+        <>
+          <button
+            onClick={() => setPage((old) => Math.max(old - 1, 1))}
+            disabled={page === 1}
+          >
+            Previous Page
+          </button>
+          {page}
+          <button
+            onClick={() => {
+              setPage((old) => (data?.next ? old + 1 : old));
+            }}
+            disabled={isPreviousData || !data?.next}
+          >
+            Next Page
+          </button>
+          <div>
+            {data.results.map((planet) => (
+              <Planet key={planet.name} planet={planet} />
+            ))}
+          </div>
+        </>
       )}
+      {
+        // Since the last page's data potentially sticks around between page requests,
+        // we can use `isFetching` to show a background loading
+        // indicator since our `status === 'loading'` state won't be triggered
+        isFetching ? <span> Loading...</span> : null
+      }{" "}
     </div>
   );
 };
