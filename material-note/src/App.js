4dd666045f49bfef4cda65368fc5cc19f6689dee
solygambas <51904909+solygambas@users.noreply.github.com>
2021-05-10 07:33:13 +0200

    add custom theme

--- material-note/src/App.js
+++ material-note/src/App.js
@@ -1,19 +1,43 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import Notes from './pages/Notes'
-import Create from './pages/Create'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core";
+
+import Notes from "./pages/Notes";
+import Create from "./pages/Create";
+import { purple } from "@material-ui/core/colors";
+
+// see: https://material-ui.com/customization/default-theme/#default-theme
+// https://material-ui.com/customization/color/#color
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#fefefe",
+    },
+    secondary: purple,
+  },
+  typography: {
+    fontFamily: "'Quicksand', sans-serif",
+    fontWeightLight: 400,
+    fontWeightRegular: 500,
+    fontWeightMedium: 600,
+    fontWeightBold: 700,
+  },
+});
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <Notes />
-        </Route>
-        <Route path="/create">
-          <Create />
-        </Route>
-      </Switch>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Notes />
+          </Route>
+          <Route path="/create">
+            <Create />
+          </Route>
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
 }
 
--- material-note/src/pages/Create.js
+++ material-note/src/pages/Create.js
@@ -1,3 +1,33 @@
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import Container from "@material-ui/core/Container";
+import KeyboardArrowRightIcon from "@material-ui/icons/KeyboardArrowRight";
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles({});
+
 export default function Create() {
-  return <div>Create page</div>;
+  const classes = useStyles();
+
+  return (
+    <Container>
+      <Typography
+        component="h2"
+        variant="h6"
+        color="textSecondary"
+        gutterBottom
+      >
+        Create a New Note
+      </Typography>
+      <Button
+        onClick={() => console.log("you clicked me")}
+        type="submit"
+        color="secondary"
+        variant="contained"
+        endIcon={<KeyboardArrowRightIcon />}
+      >
+        Submit
+      </Button>
+    </Container>
+  );
 }
