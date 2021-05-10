0174f77b95308f526cc6b013511936dd371da2c2
solygambas <51904909+solygambas@users.noreply.github.com>
2021-05-10 18:51:38 +0200

    add responsive grid

--- material-note/src/pages/Notes.js
+++ material-note/src/pages/Notes.js
@@ -1,4 +1,7 @@
 import { useState, useEffect } from "react";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import Container from "@material-ui/core/Container";
 
 export default function Notes() {
   const [notes, setNotes] = useState([]);
@@ -10,10 +13,14 @@ export default function Notes() {
   }, []);
 
   return (
-    <div>
-      {notes.map((note) => (
-        <p key={note.id}>{note.title}</p>
-      ))}
-    </div>
+    <Container>
+      <Grid container>
+        {notes.map((note) => (
+          <Grid key={note.id} item xs={12} sm={6} md={3}>
+            <Paper>{note.title}</Paper>
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
   );
 }
