79fcb64919e947fd4b3dff6d7801a8511d527493
solygambas <51904909+solygambas@users.noreply.github.com>
2021-08-14 19:30:37 +0200

    add methods and attributes

--- testing-react-app/src/App.test.js
+++ testing-react-app/src/App.test.js
@@ -1,7 +1,18 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+// methods
+// getBy, findBy and queryBy: 1 result only (error otherwise)
+// getAllBy, findAllBy, queryAllBy: array of result(s)
+// findBy and findAllBy to use async/await
+// queryBy null when no result, queryAllBy array if no match
+
+// attributes
+// accessible by users: getByRole, getByLabelText, getByPlaceholderText, getByText
+// semantic queries for screen readers: getByAltText, getByTitle
+// test id in last resort: getByTestId
+
+test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
