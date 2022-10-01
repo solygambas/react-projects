1d58505009ce9a8529de6c9ec8d3efd471d6a24f
solygambas <51904909+solygambas@users.noreply.github.com>
2022-10-01 19:51:27 +0200

    add lexical setup

---
+++ playground/text-editor/README.md
@@ -0,0 +1,5 @@
+# Text Editor
+
+A quick intro to Lexical using React.
+
+Based on [Lexical Documentation](https://github.com/facebook/lexical#getting-started-with-react) (20222).
---
+++ playground/text-editor/src/App.js
@@ -0,0 +1,15 @@
+import Editor from "./Editor";
+
+function App() {
+  return (
+    <div>
+      <header>
+        <div>
+          <Editor />
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
