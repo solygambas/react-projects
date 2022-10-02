319b403765edaac6b12c8a8313e115fd9d5dde8a
solygambas <51904909+solygambas@users.noreply.github.com>
2022-10-02 09:22:20 +0200

    fix example

--- playground/text-editor/src/index.js
+++ playground/text-editor/src/index.js
@@ -6,9 +6,9 @@ import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React>
+  <React.StrictMode>
     <App />
-  </React>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
