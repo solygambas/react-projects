a04e7a47259d6005250bc4854e07396baa62b323
solygambas <51904909+solygambas@users.noreply.github.com>
2022-10-16 15:31:20 +0200

    add TaskList

--- playground/03-storybook-intro/README.md
+++ playground/03-storybook-intro/README.md
@@ -5,9 +5,9 @@ A quick intro to learn to create UI components with React and Storybook.
 ## Features
 
 - setting up Storybook with React.
-- building a simple taskbox component in isolation.
+- building a simple Task component in isolation.
 - catching accessibility issues.
-- assembling a composite component.
+- assembling a composite TaskList component.
 - learning how to wire in data.
 - constructing a screen.
 - testing UI components.
---
+++ playground/03-storybook-intro/src/components/TaskList.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import Task from "./Task";
+import PropTypes from "prop-types";
+
+export default function TaskList({ loading, tasks, onPinTask, onArchiveTask }) {
+  const events = {
+    onPinTask,
+    onArchiveTask,
+  };
+
+  const LoadingRow = (
+    <div className="loading-item">
+      <span className="glow-checkbox" />
+      <span className="glow-text">
+        <span>Loading</span> <span>cool</span> <span>state</span>
+      </span>
+    </div>
+  );
+
+  if (loading) {
+    return (
+      <div className="list-items" data-testid="loading" key="loading">
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
+      </div>
+    );
+  }
+
+  if (tasks.length === 0) {
+    return (
+      <div className="list-items" data-testid="empty" key="empty">
+        <div className="wrapper-message">
+          <span className="icon-check" />
+          <p className="title-message">You have no tasks</p>
+          <p className="subtitle-message">Sit back and relax</p>
+        </div>
+      </div>
+    );
+  }
+
+  const tasksInOrder = [
+    ...tasks.filter((t) => t.state === "TASK_PINNED"),
+    ...tasks.filter((t) => t.state !== "TASK_PINNED"),
+  ];
+
+  return (
+    <div className="list-items">
+      {tasksInOrder.map((task) => (
+        <Task key={task.id} task={task} {...events} />
+      ))}
+    </div>
+  );
+}
+
+TaskList.propTypes = {
+  loading: PropTypes.bool,
+  tasks: PropTypes.arrayOf(Task.propTypes.task).isRequired,
+  onPinTask: PropTypes.func,
+  onArchiveTask: PropTypes.func,
+};
+
+TaskList.defaultProps = {
+  loading: false,
+};
