d52cbeaaf89e7bc4e69d2a694aa2f69a9a56aea8
solygambas <51904909+solygambas@users.noreply.github.com>
2022-10-17 18:15:01 +0200

    add mock service worker

--- playground/03-storybook-intro/.storybook/preview.js
+++ playground/03-storybook-intro/.storybook/preview.js
@@ -1,4 +1,9 @@
 import "../src/index.css";
+import { initialize, mswDecorator } from "msw-storybook-addon";
+
+initialize();
+
+export const decorators = [mswDecorator];
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
--- playground/03-storybook-intro/src/components/InboxScreen.stories.jsx
+++ playground/03-storybook-intro/src/components/InboxScreen.stories.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { Provider } from "react-redux";
+import { rest } from "msw";
 
 import InboxScreen from "./InboxScreen";
+import { MockedState } from "./TaskList.stories";
 import store from "../lib/store";
-
-import { Provider } from "react-redux";
 
 export default {
   component: InboxScreen,
@@ -14,4 +15,29 @@ export default {
 const Template = () => <InboxScreen />;
 
 export const Default = Template.bind({});
+Default.parameters = {
+  msw: {
+    handlers: [
+      rest.get(
+        "https://jsonplaceholder.typicode.com/todos?userId=1",
+        (_, res, ctx) => {
+          return res(ctx.json(MockedState.tasks));
+        }
+      ),
+    ],
+  },
+};
+
 export const Error = Template.bind({});
+Error.parameters = {
+  msw: {
+    handlers: [
+      rest.get(
+        "https://jsonplaceholder.typicode.com/todos?userId=1",
+        (_, res, ctx) => {
+          return res(ctx.status(403));
+        }
+      ),
+    ],
+  },
+};
