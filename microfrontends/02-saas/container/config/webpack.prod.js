61c61a47441863715059d3983b5d1c31cecb7a44
solygambas <51904909+solygambas@users.noreply.github.com>
2022-11-28 08:31:53 +0100

    fix marketing prod config

--- microfrontends/02-saas/container/config/webpack.prod.js
+++ microfrontends/02-saas/container/config/webpack.prod.js
@@ -15,7 +15,7 @@ const prodConfig = {
     new ModuleFederationPlugin({
       name: "container",
       remotes: {
-        marketing: `marketin@${domain}/marketing/latest/remoteEntry.js`,
+        marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`,
       },
       shared: packageJson.dependencies,
     }),
